c9b494aae884d60f71362df7e9f0128521433774
anegin <anegamega@gmail.com>
2016-11-23 18:01:33 +0200

    Add mySort array method: stage(sorting strings)

--- 03-homework/sort.js
+++ 03-homework/sort.js
@@ -1,1 +1,28 @@
-Array.prototype.mySort = function (compareFunction) {};
+Array.prototype.mySort = function (compareFunction) {
+    var arr = Object(this),
+    len = arr.length - 1,
+        i, swapped;
+
+    do {
+        swapped = false;
+        for (i = 0; i < len; i++) {
+            if (typeof compareFunction == 'function') {
+                if (compareFunction(arr[i], arr[i + 1]) > 0) {
+                    console.log('arr[i]' + arr[i]);
+                    console.log('arr[i + 1]' + arr[i + 1]);
+                    var temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }   
+            } else if (String(arr[i]) > String(arr[i + 1])) {
+                var temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return arr;
+};
