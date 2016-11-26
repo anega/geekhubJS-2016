895dc73a63bfbb663e685bd31d278dc04bb3865d
anega <anegamega@gmail.com>
2016-11-26 20:32:06 +0200

    Add ToDo app logic: remove, select, unselect, show all todos count, show selected todos count

--- 05-homework/script.js
+++ 05-homework/script.js
@@ -1,20 +1,39 @@
 var input = document.getElementById('todo-text'),
     todoList = document.getElementById('todo-list'),
-    removeBtn = document.getElementById('remove-todo'),
     allTodos = document.getElementById('all-todos'),
-    totalTodoCount = 0;
+    selectedTodos = document.getElementById('selected-todos'),
+    totalTodoCount = 0,
+    selectedCount = 0;
 
 // Add todoItem logic
 function addTodo() {
     var trimmedTodoText = input.value.trim(),
         textNode = document.createTextNode(trimmedTodoText),
         checkbox = document.createElement('input'),
+        removeBtn = document.createElement('button'),
         todoLi = document.createElement('li');
     checkbox.type = 'checkbox';
+    removeBtn.type = 'button';
+    removeBtn.innerHTML = 'Remove todo';
+
+    // Remove todoItem logic
+    removeBtn.onclick = function (event) {
+        var removedTodo = event.target.parentElement,
+            isSelected = removedTodo.getElementsByTagName('input')[0].checked;
+        removedTodo.parentElement.removeChild(removedTodo);
+        allTodos.innerHTML = --totalTodoCount;
+        if (isSelected) selectedTodos.innerHTML = --selectedCount;
+    };
+
+    checkbox.addEventListener('change', function (event) {
+        selectedCount = this.checked ? ++selectedCount : selectedCount > 0 ? --selectedCount : selectedCount;
+        selectedTodos.innerHTML = selectedCount;
+    });
 
     if (trimmedTodoText) {
         todoLi.appendChild(checkbox);
         todoLi.appendChild(textNode);
+        todoLi.appendChild(removeBtn);
         todoList.appendChild(todoLi);
         allTodos.innerHTML = ++totalTodoCount;
     }
@@ -26,5 +45,3 @@ function addTodo() {
 document.addEventListener('keyup', function (event) {
     if (event.keyCode == 13) addTodo();
 });
-
-//
